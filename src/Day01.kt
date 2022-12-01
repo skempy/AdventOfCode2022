9e0a60fc549bc5013b22628f23a598845e680d05
skempy <scottkemp28@gmail.com>
2022-12-01 10:17:34 +0000

    [Day01] - Refactor and new util method

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,37 +1,26 @@
 fun main() {
 
-    fun countCaloriesFrom(input: List<String>, listOfElves: MutableList<Int>) {
-        var elf = 0
-        input.forEach {
-            elf = when {
-                it.isNotBlank() -> { elf.plus(it.toInt()) }
-                else -> { 0.also { listOfElves.add(elf) } }
-            }
-        }
-        listOfElves.add(elf)
+    data class Elf(val calories: List<Int>)
+
+    fun part1(input: String): Int {
+        val elves = input.split("\n\n").map { group -> Elf(group.split("\n").map { it.toInt() }) }
+        return elves.maxOf { it.calories.sum() }
     }
 
-    fun part1(input: List<String>): Int {
-        val listOfElves = mutableListOf<Int>()
-        countCaloriesFrom(input, listOfElves)
-        return listOfElves.maxOf { it }
-    }
-
-    fun part2(input: List<String>): Int {
-        val listOfElves = mutableListOf<Int>()
-        countCaloriesFrom(input, listOfElves)
-        return listOfElves.sortedDescending().take(3).sum()
+    fun part2(input: String): Int {
+        val elves = input.split("\n\n").map { group -> Elf(group.split("\n").map { it.toInt() }) }
+        return elves.map { it.calories.sum() }.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInputAsString("Day01_test")
     println("TestPart1: ${part1(testInput)}")
     println("TestPart1: ${part2(testInput)}")
 
     check(part1(testInput) == 24000)
     check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = readInputAsString("Day01")
     println("Actual Part1: ${part1(input)}")
     println("Actual Part2: ${part2(input)}")
 }
