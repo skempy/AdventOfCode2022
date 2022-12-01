d472e0b8f7f4138eaf8b1a5bbc43163b8e490ce5
skempy <scottkemp28@gmail.com>
2022-12-01 09:29:31 +0000

    [Day01] - Not a one liner :(

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,37 @@
 fun main() {
+
+    fun countCaloriesFrom(input: List<String>, listOfElves: MutableList<Int>) {
+        var elf = 0
+        input.forEach {
+            elf = when {
+                it.isNotBlank() -> { elf.plus(it.toInt()) }
+                else -> { 0.also { listOfElves.add(elf) } }
+            }
+        }
+        listOfElves.add(elf)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val listOfElves = mutableListOf<Int>()
+        countCaloriesFrom(input, listOfElves)
+        return listOfElves.maxOf { it }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val listOfElves = mutableListOf<Int>()
+        countCaloriesFrom(input, listOfElves)
+        return listOfElves.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println("TestPart1: ${part1(testInput)}")
+    println("TestPart1: ${part2(testInput)}")
+
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Actual Part1: ${part1(input)}")
+    println("Actual Part2: ${part2(input)}")
 }
