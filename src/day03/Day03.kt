14df7344e0fc2ae0a4dafbc1cfdcf03879ea1118
skempy <scottkemp28@gmail.com>
2022-12-04 09:49:20 +0000

    [Day03] - Cleanup

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -2,75 +2,23 @@ package day03
 
 import readInputAsList
 
-enum class Alphabet(val lower: Int, val upper: Int) {
-    A(1, 27),
-    B(2, 28),
-    C(3, 29),
-    D(4, 30),
-    E(5, 31),
-    F(6, 32),
-    G(7, 33),
-    H(8, 34),
-    I(9, 35),
-    J(10, 36),
-    K(11, 37),
-    L(12, 38),
-    M(13, 39),
-    N(14, 40),
-    O(15, 41),
-    P(16, 42),
-    Q(17, 43),
-    R(18, 44),
-    S(19, 45),
-    T(20, 46),
-    U(21, 47),
-    V(22, 48),
-    W(23, 49),
-    X(24, 50),
-    Y(25, 51),
-    Z(26, 52)
-}
-
 fun main() {
 
+    var priority = 1
+    val priorities: Map<Char, Int> = (('a'..'z') + ('A'..'Z')).associateWith { priority++ }
+
     fun part1(input: List<String>): Int {
-        val compartments = input.map { it.chunked(it.length / 2) }.map { Pair(it[0], it[1]) }
-
-        val sharedItems = mutableListOf<Char>()
-
-        compartments.forEach {
-            sharedItems.add(it.second.first { c -> it.first.contains(c) })
-        }
-
-        return sharedItems.sumOf { char ->
-            val score = Alphabet.valueOf(char.uppercase())
-            if (char.isUpperCase()) {
-                score.upper
-            } else {
-                score.lower
-            }
-        }
+        return input
+            .map { it.chunked(it.length / 2) }
+            .map { Pair(it[0].toSet(), it[1].toSet()) }
+            .sumOf { priorities[(it.first intersect it.second).single()]!! }
     }
 
     fun part2(input: List<String>): Int {
-        val compartments = input.chunked(3)
-
-        val sharedItems = mutableListOf<Char>()
-
-        compartments.forEach {
-            val list1 = it[1].filter { c -> it[0].contains(c) }.toList()
-            val list2 = it[2].filter { c -> it[1].contains(c) }.toList()
-            sharedItems.add(list1.first { c -> list2.contains(c) })
-        }
-
-        return sharedItems.sumOf { char ->
-            val score = Alphabet.valueOf(char.uppercase())
-            if (char.isUpperCase()) {
-                score.upper
-            } else {
-                score.lower
-            }
-        }
+        return input
+            .map { it.toSet() }
+            .chunked(3)
+            .sumOf { priorities[(it[0] intersect it[1] intersect it[2]).single()]!! }
     }
 
     // test if implementation meets criteria from the description, like:
@@ -86,5 +34,6 @@ fun main() {
     println("Actual Part2: ${part2(input)}")
 
     check(part1(input) == 7811)
-    check(part2(input) == 13022)
+    check(part2(input) == 2639)
 }
+
