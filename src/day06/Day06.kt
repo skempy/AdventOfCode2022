71b94ce747852356bbf25b6aaf3628160d53707a
skempy <scottkemp28@gmail.com>
2022-12-06 08:56:04 +0000

    [Day06] - Simple with Kotlin

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -4,25 +4,47 @@ import readInputAsList
 
 fun main() {
 
-    fun part1(input: List<String>): String {
-        return "blurt"
+    fun findMarker(
+        input: List<String>,
+        listOfNonRepeatingChars: MutableMap<Int, String>,
+        sizeOfPacket: Int,
+    ) {
+        input.first()
+            .windowed(sizeOfPacket)
+            .forEachIndexed { index, packet ->
+                if (packet.toSet().size == sizeOfPacket) {
+                    listOfNonRepeatingChars[index.plus(sizeOfPacket)] = packet
+                }
+            }
     }
 
-    fun part2(input: List<String>): String {
-        return "blurt"
+    fun part1(input: List<String>): Int {
+        val listOfNonRepeatingChars = mutableMapOf<Int, String>()
+
+        findMarker(input, listOfNonRepeatingChars, 4)
+
+        return listOfNonRepeatingChars.keys.min()
+    }
+
+    fun part2(input: List<String>): Int {
+        val listOfNonRepeatingChars = mutableMapOf<Int, String>()
+
+        findMarker(input, listOfNonRepeatingChars, 14)
+
+        return listOfNonRepeatingChars.keys.min()
     }
 
 // test if implementation meets criteria from the description, like:
     val testInput = readInputAsList("Day06", "_test")
     println("Test Part1: ${part1(testInput)}")
-/*    check(part1(testInput) == "CMZ")
+    check(part1(testInput) == 7)
 
-    val input = readInputAsList("Day05")
+    val input = readInputAsList("Day06")
     println("Actual Part1: ${part1(input)}")
-    check(part1(input) == "VGBBJCRMN")
+    check(part1(input) == 1804)
 
     println("Test Part2: ${part2(testInput)}")
-    check(part2(testInput) == "MCD")
+    check(part2(testInput) == 19)
     println("Actual Part2: ${part2(input)}")
-    check(part2(input) == "LBBVJBRMH")*/
+    check(part2(input) == 2508)
 }
