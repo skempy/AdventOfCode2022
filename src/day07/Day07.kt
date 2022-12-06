6748f36ff4cb838bae1f338de7a16e780824eccd
skempy <scottkemp28@gmail.com>
2022-12-06 09:31:48 +0000

    [Day07] - Prep

---
+++ src/day07/Day07.kt
@@ -0,0 +1,28 @@
+package day07
+
+import readInputAsList
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+// test if implementation meets criteria from the description, like:
+    val testInput = readInputAsList("Day076", "_test")
+    println("Test Part1: ${part1(testInput)}")
+    check(part1(testInput) == 7)
+//
+//    val input = readInputAsList("Day07")
+//    println("Actual Part1: ${part1(input)}")
+//    check(part1(input) == 1804)
+//
+//    println("Test Part2: ${part2(testInput)}")
+//    check(part2(testInput) == 19)
+//    println("Actual Part2: ${part2(input)}")
+//    check(part2(input) == 2508)
+}
---
+++ src/day07/Day07.md
@@ -0,0 +1,11 @@
+# https://adventofcode.com/2022/day/7
+
+<div align="center">
+    <div style="display: flex; align-items: flex-start;">
+        <h1 style="text-align: center;">
+            <br>
+        <img src="Day07.png" width="500" alt="advent-of-code">
+            <br>
+        </h1>
+    </div>
+</div>
